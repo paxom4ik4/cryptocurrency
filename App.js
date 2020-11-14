6b67347c6b467266bb97ab079cc4ba4d141c7bf5
paxom4ik4 <pasha.zelenko001@gmail.com>
2020-11-14 16:59:14 +0300

    init: crypto app

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './src/components';
+import CryptoContainer from './src/components/CryptoContainer'
+import { Provider } from 'react-redux';
+
+import Store from './src/Store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={Store}>
+      <View>
+        <Header />
+        <CryptoContainer />
+      </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
